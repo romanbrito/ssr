520979ddf8bac450d76b4281d2d0b985169b1bbc
Roman Brito <romanbrito1@gmail.com>
2018-11-30 10:42:18 -0600

    adding data for props on index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,28 @@
+import React, {Component} from 'react'
 import './index.css';
 import Card from './Card';
+import data from '../data/data.json';
 
-export default () => (
-  <div className="App">
-    <header className="App-header">
-      <img src="/static/logo.png" className="static-logo" alt="logo"/>
-    </header>
-      <div className="Grid">
-        <Card />
-        <Card />
-        <Card />
+export default class Index extends Component {
+  static async getInitialProps() {
+    return { cards: data}
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="/static/logo.png" className="static-logo" alt="logo"/>
+        </header>
+        <div className="Grid">
+          { this.props.cards.map(card => (
+            <Card key={card.id} />
+          ))}
+        </div>
       </div>
-  </div>
-);
+    )
+  }
+
+};
 
